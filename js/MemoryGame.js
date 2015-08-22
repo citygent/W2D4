c02f2c4a8db03267f30390920d478d3e21db68c2
James White <cyberyt@gmail.com>
2015-08-22 22:54:40 +0100

    tileflip progress

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -8,26 +8,39 @@ var gameTiles = [];
 var possibleTiles = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 
-
-
-function setupBoard() {
-  getGameTiles(16); 
-  gameTiles = randomiseBoard(gameTiles);
-  // this forloop goes through array of selested and randomised tiles and builds divs for them on page. 
-  for (i=0; i < gameTiles.length; i++) {  // have a look at jquery.each / $.each(array, function(){} when refactoring. 
-    // $('<div class="tile" data-tile="'+ gameTiles[i] +'" </div>').appendTo('#board'); // this looks funny in dev tools, investigate why. 
-    $("<div class='tile' data-tile='"+ gameTiles[i] +"' </div>").appendTo("#board"); // this looks funny in dev tools, investigate why. 
-  }
-  addEventListeners()
-}
-
-function addEventListeners() {
-  $('.tile').on('click', function(){
-    console.log('clickerino');
+function eventListeners(){
+  $('.tile').on('click', function(event) {
+    var id = ($(this));
+    console.log(id);
+    console.log(this.id);
+    var tileimg = ($(this).data("tile"))//From docs: var mydata = $( "#mydiv" ).data();
+    console.log(tileimg)
+    flipTile(id, tileimg);
   });
 }
 
-//selects random tile from possilbe list, pushes two of each to array. 
+function flipTile(id, tileimg){ // tells tile to display
+  $(id).html(tileimg);
+}
+
+function setupBoard() {
+  getGameTiles(16); 
+  console.log("Selected Tiles:"+gameTiles);
+  console.log("Amount of Selected Tiles:"+gameTiles.length);
+  console.log("Unselected Tiles:"+possibleTiles);
+  gameTiles = randomiseBoard(gameTiles);
+  console.log("Shuffled Game Tiles:"+gameTiles);
+  console.log("Amount of Shuffled Tiles:"+gameTiles.length);
+  // this forloop goes through array of selested and randomised tiles and builds divs for them on page. 
+  for (i=0; i < gameTiles.length; i++) {  // have a look at jquery.each / $.each(array, function(){} when refactoring. 
+ // $('<div class="tile" data-tile="'+ gameTiles[i] +'" </div>').appendTo('#board'); // this looks funny in dev tools, investigate why. 
+    $("<div class='tile' id='"+i+"' data-tile='"+ gameTiles[i] +"'></div>").appendTo("#board"); // this looks funny in dev tools, investigate why. 
+  }
+  eventListeners();
+}
+
+
+//selects random tile from possilbes list, pushes two of each to array. 
 function getGameTiles(tiles) {
   for(i=0; i < (tiles/2); i++) { //needs /2 because places two of each innit.
     var randomTile = possibleTiles[Math.floor(Math.random()*possibleTiles.length)];
@@ -38,17 +51,13 @@ function getGameTiles(tiles) {
   }
 }
 
-console.log("Selected Tiles:"+gameTiles);
-console.log("Unselected Tiles:"+possibleTiles);
-console.log("Amount of Tiles:"+gameTiles.length);
-
-function randomiseBoard(gameBoard) {
+function randomiseBoard(arrayOfTiles) {
   var shuffled = [];
-  while (gameBoard.length > 0) { // Whilst tiles left on unshuffled board...
-		tile = gameBoard[Math.floor(Math.random() * gameBoard.length)]; // Pick a random tile
+  while (arrayOfTiles.length > 0) { // Whilst tiles left on unshuffled board...
+		tile = arrayOfTiles[Math.floor(Math.random() * arrayOfTiles.length)]; // Pick a random tile
 		shuffled.push(tile); // push it to resulting board
-		gameBoard.splice(gameBoard.indexOf(tile), 1); // make sure can't be called again by removing from array.
+		arrayOfTiles.splice(arrayOfTiles.indexOf(tile), 1); // make sure can't be called again.
 	  }
 	  return shuffled;
 }
-console.log("Shuffled Game Tiles:"+gameTiles);
+
