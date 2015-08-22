99f98e6c0a981cb880329db2eb03be37c1f3fd0e
James White <cyberyt@gmail.com>
2015-08-22 17:50:34 +0100

    started setup(), changed some func/var names

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -1,32 +1,35 @@
-console.log("PRIORITY ONE\nENSURE RETURN OF ORGANISM FOR ANALYSIS.\nALL OTHER CONSIDERATIONS SECONDARY.\nCREW EXPENDABLE.");
+$(document).ready(function(){
+  console.log("PRIORITY ONE\nENSURE RETURN OF ORGANISM FOR ANALYSIS.\nALL OTHER CONSIDERATIONS SECONDARY.\nCREW EXPENDABLE.");
+  setup();
+})
+
 // build game for 16 tiles first, if time refactor for any size.
-var gameBoard = [];
+var gameTiles = [];
 var possibleTiles = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-//selects random tile from possilbe list, pushes two of each to gameboard. 
-function buildBoard(tiles) {
+function setup() {
+  getGameTiles(16);
+  gameTiles = randomiseBoard(gameTiles);
+  for (i=0; i < gameTiles.length; i++) {
+    $('<div class="tile" data-tile="'+ gameTiles[i] +'" </div>').appendTo('#board'); // this looks funny in dev tools, investigate why. 
+  }
+}
+
+
+//selects random tile from possilbe list, pushes two of each to array. 
+function getGameTiles(tiles) {
   for(i=0; i < (tiles/2); i++) { //needs /2 because places two of each innit.
     var randomTile = possibleTiles[Math.floor(Math.random()*possibleTiles.length)];
     // console.log(randomTile);
-    gameBoard.push(randomTile);
-    gameBoard.push(randomTile); 
+    gameTiles.push(randomTile);
+    gameTiles.push(randomTile); 
     possibleTiles.splice(possibleTiles.indexOf(randomTile),1);
   }
 }
 
-buildBoard(16);
-console.log("Game Board:"+gameBoard);
+console.log("Selected Tiles:"+gameTiles);
 console.log("Unselected Tiles:"+possibleTiles);
-console.log("Amount of Tiles:"+gameBoard.length);
-
-// function randomiseBoard(aGameBoard) {
-// 	var shuffled = [];
-// 	while (aGameBoard.length > 0); { // Whilst tiles left on unshuffled board...
-// 		randomTile = Math.floor(Math.random()*aGameBoard.length--); // Pick a random tile
-// 		shuffled.push(aGameBoard.splice(randomTile,1)[0]);  //push it to shuffled board, removing it from original.
-// 	}
-// 	return shuffled; // return shuffled board on function call.
-// } Returning NaN. Need to remember to call values not indexes. Trying to be too clever on line 26. 
+console.log("Amount of Tiles:"+gameTiles.length);
 
 function randomiseBoard(gameBoard) {
   var shuffled = [];
@@ -38,6 +41,5 @@ function randomiseBoard(gameBoard) {
 	  return shuffled;
 }
 
-gameBoard = randomiseBoard(gameBoard);
-console.log("Shuffled Game Board:"+gameBoard);
-console.log("Amount of Shuffled Tiles:"+gameBoard.length);
+console.log("Shuffled Game Tiles:"+gameTiles);
+console.log("Amount of Shuffled Tiles:"+gameTiles.length);
