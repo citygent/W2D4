f83baabe7179c5ae24cb192cb369d74c5e66fd4f
James White <cyberyt@gmail.com>
2015-08-22 13:22:37 +0100

    build board function added

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -1,3 +1,17 @@
-console.log("PRIORITY ONE\nENSURE RETURN OF ORGANISM FOR ANALYSIS.\nALL OTHER CONSIDERATIONS SECONDARY.\nCREW EXPENDABLE.")
+console.log("PRIORITY ONE\nENSURE RETURN OF ORGANISM FOR ANALYSIS.\nALL OTHER CONSIDERATIONS SECONDARY.\nCREW EXPENDABLE.");
+// build game for 16 tiles first, if time refactor for any size.
+var gameBoard = [];
+var possibleTiles = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-var MemoryGame = MemoryGame || {};
+//selects random tile from possilbe list, pushes two of each to gameboard. 
+function buildBoard(rows, cols) {
+	for(i=0; i < cols; i++) {
+		for (j=0; j < rows; j++) {
+			var randomTile = possibleTiles[Math.floor(Math.random()*possibleTiles.length)];
+			gameBoard.push(randomTile);
+			gameBoard.push(randomTile); 
+		}
+	}
+}
+
+buildBoard(4,4);
